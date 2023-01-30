057960a215059ce59ce58adbc675fe6c20eaebdd
golubeva-webmaster <golubeva-webmaster@gmail.com>
2023-01-30 16:33:58 +0300

    feat: NCM-styles edit styles

---
+++ src/runtime/components/dashboard/DashboardPrinterActionTitle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="action-title">
+    <slot>
+      <v-col cols="12" :class="`${color}--text title`">
+        {{ title }}
+      </v-col>
+    </slot>
+    <slot name="description">
+      <v-col cols="12" class="subtitle-2 text-truncate">
+        {{ description }}
+      </v-col>
+    </slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "nuxt-property-decorator";
+
+@Component({
+  name: "DashboardPrinterActionTitle",
+})
+export default class DashboardPrinterActionTitle extends Vue {
+  @Prop({ type: String, default: "" }) title!: string;
+  @Prop({ type: String, default: "" }) color!: string;
+  @Prop({ type: String, default: "" }) description!: string;
+}
+</script>
+
+<style>
+.action-title {
+  display: contents;
+}
+</style>
---
+++ src/runtime/components/dashboard/LastJobStatus.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-radio-group
+    :value="value"
+    @change="$emit('change', $event)"
+    column
+    class="statusSelect"
+  >
+    <template v-for="status in items">
+      <v-radio
+        :key="status.value"
+        :value="status.value"
+        active-class="font-weight-bold"
+        :color="value === status.value ? status.color : ''"
+      >
+        <template v-slot:label>
+          <v-icon
+            v-if="status.icon"
+            :color="value === status.value ? status.color : ''"
+            >{{ status.icon }}</v-icon
+          >
+          <font :color="value === status.value ? status.color : ''">
+            {{ status.text }}
+          </font>
+        </template>
+      </v-radio>
+    </template>
+  </v-radio-group>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Model, Prop } from "nuxt-property-decorator";
+
+@Component({
+  name: "LastJobStatus",
+})
+export default class LastJobStatus extends Vue {
+  @Model("change", { default: "", type: String }) value!: string;
+  @Prop({ type: Array, default: () => [] }) items!: {
+    text: string;
+    color?: string;
+    icon?: string;
+    value: string;
+  }[];
+}
+</script>
+
+<style>
+.statusSelect .v-input--selection-controls__input {
+  display: none;
+}
+</style>
