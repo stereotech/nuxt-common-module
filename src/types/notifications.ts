5d81412a03ce8894b16cb1c2121ff820c4a7a6e5
Marina <60580475+golubeva-webmaster@users.noreply.github.com>
2023-06-08 14:37:10 +0300

    Update notifications.ts

--- src/types/notifications.ts
+++ src/types/notifications.ts
@@ -3,6 +3,7 @@ export type MessageType = 'success' | 'info' | 'error' | 'warning'
 export interface Message {
   type: MessageType
   text: string
+  dialog: boolean
   actions?: MessageAction[]
   timeout?: number
 }
